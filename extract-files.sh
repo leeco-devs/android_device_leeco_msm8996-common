2ed5de2fc4cb5e723792cac084b0ccb032405035
mosimchah <mosimchah@gmail.com>
2020-05-24 22:25:35 -0400

    msm8996-common: move ims libs to product
    Change-Id: I09098d7445855dcf8b7b72764c1577d2ef4c0143

--- extract-files.sh
+++ extract-files.sh
@@ -72,6 +72,11 @@ function blob_fixup() {
         sed -i -e 's|name=\"android.hidl.manager-V1.0-java|name=\"android.hidl.manager@1.0-java|g' "${2}"
         ;;
 
+    # move ims libs to product
+    product/etc/permissions/com.qualcomm.qti.imscmservice.xml)
+        sed -i -e 's|file="/system/framework/|file="/product/framework/|g' "${2}"
+        ;;
+
     # kang vulkan from LA.UM.8.6.r1-01900-89xx.0
     vendor/lib/hw/vulkan.msm8996.so | vendor/lib64/hw/vulkan.msm8996.so)
         sed -i -e 's|vulkan.msm8953.so|vulkan.msm8996.so|g' "${2}"
